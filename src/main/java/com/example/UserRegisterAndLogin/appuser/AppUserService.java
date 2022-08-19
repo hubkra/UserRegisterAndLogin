69f7d18102b720a5d70ef654642d99ca1cc79c29
Hubert Kras <mrkrashubert@gmail.com>
2022-08-19 13:15:29 +0200

    Tests adding user to database, check if we got exception if email is already taken

--- src/main/java/com/example/UserRegisterAndLogin/appuser/AppUserService.java
+++ src/main/java/com/example/UserRegisterAndLogin/appuser/AppUserService.java
@@ -1,5 +1,6 @@
 package com.example.UserRegisterAndLogin.appuser;
 
+import com.example.UserRegisterAndLogin.seciurity.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AppUserService(AppUserRepository appUserRepository) {
+    public AppUserService(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
         this.appUserRepository = appUserRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -27,7 +30,14 @@ public class AppUserService implements UserDetailsService {
         if(userExist){
             throw new IllegalStateException("Email already taken");
         }
+        String encodedPassword = passwordEncoder.bCryptPasswordEncoder().encode((appUser.getPassword()));
 
-        return  "";
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        //TODO: SEND CONFIRMATION TOKEN
+
+        return  "it works!";
     }
 }
